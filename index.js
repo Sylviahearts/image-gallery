c2b30fcaf4dc2bdc0dda0405cd5fab69142b97a9
Sylvia <114693641+Sylviahearts@users.noreply.github.com>
2023-04-20 18:18:28 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,199 @@
+//pic1
+function pic1(){
+    document.getElementById('text').style.display = 'block';
+}
+function pic1hide(){
+    document.getElementById('text').style.display = 'none';
+}
+//pic2
+function pic2(){
+    document.getElementById('text2').style.display = 'block';
+}
+function pic2hide(){
+    document.getElementById('text2').style.display = 'none';
+}
+//pic3
+function pic3(){
+    document.getElementById('text3').style.display = 'block';
+}
+function pic3hide(){
+    document.getElementById('text3').style.display = 'none';
+}
+//pic4
+function pic4(){
+    document.getElementById('text4').style.display = 'block';
+}
+function pic4hide(){
+    document.getElementById('text4').style.display = 'none';
+}
+//pic5
+function pic5(){
+    document.getElementById('text5').style.display = 'block';
+}
+function pic5hide(){
+    document.getElementById('text5').style.display = 'none';
+}
+//pic6
+function pic6(){
+    document.getElementById('text6').style.display = 'block';
+}
+function pic6hide(){
+    document.getElementById('text6').style.display = 'none';
+}
+//pic7
+function pic7(){
+    document.getElementById('text7').style.display = 'block';
+}
+function pic7hide(){
+    document.getElementById('text7').style.display = 'none';
+}
+//pic18
+function pic8(){
+    document.getElementById('text8').style.display = 'block';
+}
+function pic8hide(){
+    document.getElementById('text8').style.display = 'none';
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.getElementById('mybtn').addEventListener("click", txt);
+
+//  function txt(){
+//      document.getElementById('text').style.display = 'block';
+//    }
+//btn1
+// var a = document.getElementById("mybtn");
+// a.addEventListener("click", show);
+
+// function show() {
+//   document.getElementById("text").style.display = "block";
+// }
+
+// var b = document.getElementById("text");
+// b.addEventListener("mouseout", hide);
+
+// function hide() {
+//   document.getElementById("text").style.display = "none";
+// }
+
+// // //btn2
+// var c = document.getElementById("mybtn2");
+// c.addEventListener("click", show);
+
+// function show() {
+//   document.getElementById("text2").style.display = "block";
+// }
+
+// var d = document.getElementById("text2");
+// d.addEventListener("mouseout", hide);
+
+// function hide() {
+//   document.getElementById("text2").style.display = "none";
+// }
+
+// //btn3
+// var x = document.getElementById("mybtn3");
+// x.addEventListener("click", show);
+
+// function show() {
+//   document.getElementById("text3").style.display = "block";
+// }
+
+// var y = document.getElementById("text3");
+// y.addEventListener("mouseout", hide);
+
+// function hide() {
+//   document.getElementById("text3").style.display = "none";
+// }
+
+
+// //btn4
+// var x = document.getElementById("mybtn4");
+// x.addEventListener("click", show);
+
+// function show() {
+//   document.getElementById("text4").style.display = "block";
+// }
+
+// var y = document.getElementById("text4");
+// y.addEventListener("mouseout", hide);
+
+// function hide() {
+//   document.getElementById("text4").style.display = "none";
+// }
+
+
+// //btn5
+// var x = document.getElementById("mybtn5");
+// x.addEventListener("click", show);
+
+// function show() {
+//   document.getElementById("text5").style.display = "block";
+// }
+
+// var y = document.getElementById("text5");
+// y.addEventListener("mouseout", hide);
+
+// function hide() {
+//   document.getElementById("text5").style.display = "none";
+// }
+
+
+// //btn6
+// var x = document.getElementById("mybtn6");
+// x.addEventListener("click", show);
+
+// function show() {
+//   document.getElementById("text6").style.display = "block";
+// }
+
+// var y = document.getElementById("text6");
+// y.addEventListener("mouseout", hide);
+
+// function hide() {
+//   document.getElementById("text6").style.display = "none";
+// }
+
+
+// //btn7
+// var x = document.getElementById("mybtn7");
+// x.addEventListener("click", show);
+
+// function show() {
+//   document.getElementById("text7").style.display = "block";
+// }
+
+// var y = document.getElementById("text7");
+// y.addEventListener("mouseout", hide);
+
+// function hide() {
+//   document.getElementById("text7").style.display = "none";
+// }
+
+
+// //btn8
+// var x = document.getElementById("mybtn8");
+// x.addEventListener("click", show);
+
+// function show() {
+//   document.getElementById("text8").style.display = "block";
+// }
+
+// var y = document.getElementById("text8");
+// y.addEventListener("mouseout", hide);
+
+// function hide() {
+//   document.getElementById("text8").style.display = "none";
+// }
